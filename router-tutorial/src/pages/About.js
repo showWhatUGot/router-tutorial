89fffacff773260080c49f8211583472c0217ddc
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-08-10 17:01:42 +0900

    Home.js, About.js 생성

---
+++ router-tutorial/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>라우터 공부 프로젝트</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ router-tutorial/src/pages/Homes.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Homes = () => {
+  return (
+    <div>
+      <h1>홈</h1>
+      <p>홈 화면</p>
+    </div>
+  );
+};
+
+export default Homes;
