2ebf32ffbee1ed0aafde97fbce142fa7ef938d49
Call_Ma_Name <93004055+showWhatUGot@users.noreply.github.com>
2022-08-10 17:31:38 +0900

    쿼리스트링

--- router-tutorial/src/pages/About.js
+++ router-tutorial/src/pages/About.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const About = () => {
+  const loaction = useLocation();
+
   return (
     <div>
       <h1>소개</h1>
       <p>라우터 공부 프로젝트</p>
+      <p>쿼리스트링 : {loaction.search}</p>
     </div>
   );
 };
